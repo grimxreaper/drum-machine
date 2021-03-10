00ac542a5779fc2f1c711e12a5af4f694ab9c1bd
Zahra <hizahrazadeh@gmail.com>
2021-03-10 17:14:47 -0500

    updates to class Box

--- src/App.js
+++ src/App.js
@@ -2,35 +2,70 @@ import ReactDOM from 'react-dom';
 import React, { Component } from 'react';
 import './App.css';
 
+const sounds = [
+  {
+    key: 'Q',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    key: 'W',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    key: 'E',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    key: 'A',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    key: 'S',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    key: 'D',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    key: 'Z',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    key: 'X',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    key: 'C',
+    mp3: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  },
+]
 
-class App extends Component {
-  state = {
-    keys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
-  }
+const keys = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'];
 
-  render() {
-    const { keys } = this.state;
-    return (
-      
-        <div id="display" class="display">
-          {keys.map((key, index) => (
-            <Box text={key} key={index}/>
+const App = (props) => (
+  <div class="display" id="display">
+          {sounds.map((sound, index) => (
+            <Box text={sound.key} key={index} audio={sound.mp3}/>
           ))}
-          
         </div>
       
 
     );
-  }
-}
-
+  
 // we have a box with some text in it coming from the props
-const Box = () => (
-  <div className="box">
-    {props.text}
-  </div>
-)
-
+//converted to class based component to have state on it 
+class Box extends React.Component {
+  render() {
+    const { text, audio } = this.props; 
+    return(
+        <div className="box">
+          {text}
+          <audio src={audio} />
+        </div>
+      )
+  }
+ }
 
 export default App;
 ReactDOM.render(<App />, document.getElementById('drum-machine'));

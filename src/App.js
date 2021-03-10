6884dcc577653ef71176c6bc0ed4e770fafa61d5
Zahra <hizahrazadeh@gmail.com>
2021-03-10 16:42:02 -0500

    upd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import './App.css';
 
-const App = () => (
-  <div>Hello</div>
-);
+
+class App extends Component {
+  state = {
+    keys: ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
+  }
+
+  render() {
+    const { keys } = this.state;
+    return (
+      <div id="drum-machine">
+        <div id="display">
+          {keys.map((key, index) => (
+            <Box text={key} key={index}/>
+          ))}
+          
+        </div>
+      </div>
+
+    );
+  }
+}
+
+// we have a box with some text in it coming from the props
+const Box = () => (
+  <div className="box">
+    {props.text}
+  </div>
+)
+
 
 export default App;
 ReactDOM.render(<App />, document.getElementById('app'));
